86b727d7b77b9e9b15a726363965a180934ca011
daeho.kim <daeho.kim@phoenixdarts.com>
2024-04-08 20:05:40 +0900

    refactor : duplicate test code into function

--- src/test/java/hodaessi/toy/ExpiryDateCalculatorTest.java
+++ src/test/java/hodaessi/toy/ExpiryDateCalculatorTest.java
@@ -12,21 +12,23 @@ public class ExpiryDateCalculatorTest {
     @Test
     void if_you_pay_10000_won_the_expiration_date_will_be_one_month_later() {
         // test01
-        LocalDate billingDate = LocalDate.of(2019, 3, 1);
-        int payAmount = 10_000;
-
-        ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+        assertExpiryDate(
+                LocalDate.of(2019, 3, 1),
+                10_000,
+                LocalDate.of(2019, 4, 1)
+        );
 
         // test02
-        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
-        int payAmount2 = 10_000;
+        assertExpiryDate(
+                LocalDate.of(2019, 5, 5),
+                10_000,
+                LocalDate.of(2019, 6, 5)
+        );
+    }
 
-        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
-
-        assertEquals(LocalDate.of(2019, 6, 5), expiryDate2);
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, expiryDate);
     }
 }
