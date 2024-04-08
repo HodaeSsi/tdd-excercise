5d5f02a8f20d9e8d36780e42acd7e2a2d9838b4f
daeho.kim <daeho.kim@phoenixdarts.com>
2024-04-08 20:03:08 +0900

    test : 만원 납부시, 한달 뒤가 만료일

---
+++ src/test/java/hodaessi/toy/ExpiryDateCalculator.java
@@ -0,0 +1,9 @@
+package hodaessi.toy;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return billingDate.plusMonths(1);
+    }
+}
---
+++ src/test/java/hodaessi/toy/ExpiryDateCalculatorTest.java
@@ -0,0 +1,32 @@
+package hodaessi.toy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExpiryDateCalculatorTest {
+
+    @Test
+    void if_you_pay_10000_won_the_expiration_date_will_be_one_month_later() {
+        // test01
+        LocalDate billingDate = LocalDate.of(2019, 3, 1);
+        int payAmount = 10_000;
+
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+
+        assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+
+        // test02
+        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
+        int payAmount2 = 10_000;
+
+        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
+        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
+
+        assertEquals(LocalDate.of(2019, 6, 5), expiryDate2);
+    }
+}
